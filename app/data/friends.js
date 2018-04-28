908efaf4164f5b6f29b05f85ee4bf0e284d91c09
Jennifer Artschwager <Jennifer@artschwager.com>
2018-04-28 16:55:28 -0400

    ready to deploy

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,23 @@ var uuid = require('uuid/v4');
 var friendArray = [
 
 {   name: "Zoey Artschwager",
-    photo: "https://www.instagram.com/p/BhkyFvyFJaEOdj17anLmqJAUMjmfjwkBroH8340/?taken-by=jenn.arts",
+    photo: "https://cdn.britannica.com/700x450/99/152499-120-23D4CC5D.jpg",
+    "scores":[
+        0,
+        1,
+        4,
+        2,
+        5,
+        1,
+        2,
+        1,
+        4,
+        1
+      ],
+      id: uuid()
+  },
+  {   name: "Nadia Artschwager",
+    photo: "https://s.abcnews.com/images/US/ht_Tiny_Tim_kb_121228_wmain.jpg",
     "scores":[
         5,
         1,
@@ -20,22 +36,22 @@ var friendArray = [
       ],
       id: uuid()
   },
-  {   name: "Nadia Artschwager",
-    photo: "https://www.instagram.com/p/BhkAry9lnvG3l3TnSN9yjSVfmP-OeVcddA_Bkg0/?taken-by=jenn.arts",
+  {   name: "Jennifer Artschwager",
+    photo: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/26168869_10101016956408355_1588294270133062539_n.jpg?_nc_cat=0&oh=15ab114d10a0b0384ce6d8f884c23fd3&oe=5B4E9DD6",
     "scores":[
-        5,
+        0,
         1,
         4,
-        4,
+        2,
         5,
         1,
         2,
-        5,
+        1,
         4,
         1
       ],
       id: uuid()
-  }
+  },
 ];
 
 module.exports = friendArray;
